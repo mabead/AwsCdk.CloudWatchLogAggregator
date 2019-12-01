4876c79be8be305dc3f275c88411d69e65c0e7df
Maxime Beaudry <mabead@users.noreply.github.com>
2019-12-01 13:17:53 -0500

    add missing semicolons

--- src/AwsCdk.CloudWatchLogForwarder/Resources/SubscribeLogGroupsToKinesis.js
+++ src/AwsCdk.CloudWatchLogForwarder/Resources/SubscribeLogGroupsToKinesis.js
@@ -1,8 +1,8 @@
-﻿const AWS = require('aws-sdk')
-const cloudWatchLogs = new AWS.CloudWatchLogs()
-const arn = process.env.arn
-const role_arn = process.env.role_arn
-const prefix = process.env.prefix
+﻿const AWS = require('aws-sdk');
+const cloudWatchLogs = new AWS.CloudWatchLogs();
+const arn = process.env.arn;
+const role_arn = process.env.role_arn;
+const prefix = process.env.prefix;
 
 const subscribe = async (logGroupName) => {
     let options = {
@@ -11,22 +11,22 @@ const subscribe = async (logGroupName) => {
         filterName: 'ship-logs',
         filterPattern: '[timestamp=*Z, request_id="*-*", event]',
         roleArn: role_arn,
-    }
+    };
 
-    await cloudWatchLogs.putSubscriptionFilter(options).promise()
-}
+    await cloudWatchLogs.putSubscriptionFilter(options).promise();
+};
 
 module.exports.handler = async (event, context) => {
-    console.log(JSON.stringify(event))
+    console.log(JSON.stringify(event));
 
     // eg. /aws/lambda/logging-demo-dev-api
-    const logGroupName = event.detail.requestParameters.logGroupName
-    console.log(`log group: ${logGroupName}`)
+    const logGroupName = event.detail.requestParameters.logGroupName;
+    console.log(`log group: ${logGroupName}`);
 
     if (prefix && !logGroupName.startsWith(prefix)) {
-        console.log(`ignoring the log group [${logGroupName}] because it doesn't match the prefix [${prefix}]`)
+        console.log(`ignoring the log group [${logGroupName}] because it doesn't match the prefix [${prefix}]`);
     } else {
-        await subscribe(logGroupName)
-        console.log(`subscribed [${logGroupName}] to [${arn}]`)
+        await subscribe(logGroupName);
+        console.log(`subscribed [${logGroupName}] to [${arn}]`);
     }
-}
+};
