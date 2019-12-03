b605ef1fee861c39fa81562f3016082d4b825aff
Maxime Beaudry <mabead@users.noreply.github.com>
2019-12-03 06:51:29 -0500

    semi colons

--- src/AwsCdk.CloudWatchLogForwarder/Resources/SetExpiry.js
+++ src/AwsCdk.CloudWatchLogForwarder/Resources/SetExpiry.js
@@ -1,25 +1,25 @@
 ﻿// Code taken from here: https://github.com/theburningmonk/lambda-logging-kinesis-demo/blob/master/functions/set-retention/handler.js
 
-const AWS = require('aws-sdk')
-const cloudWatchLogs = new AWS.CloudWatchLogs()
-const retentionDays = process.env.retention_days
+const AWS = require('aws-sdk');
+const cloudWatchLogs = new AWS.CloudWatchLogs();
+const retentionDays = process.env.retention_days;
 
 const setExpiry = async (logGroupName) => {
     let params = {
         logGroupName: logGroupName,
         retentionInDays: retentionDays
-    }
+    };
 
-    await cloudWatchLogs.putRetentionPolicy(params).promise()
-}
+    await cloudWatchLogs.putRetentionPolicy(params).promise();
+};
 
 module.exports.handler = async (event, context) => {
-    console.log(JSON.stringify(event))
+    console.log(JSON.stringify(event));
 
-    const logGroupName = event.detail.requestParameters.logGroupName
-    console.log(`log group: ${logGroupName}`)
+    const logGroupName = event.detail.requestParameters.logGroupName;
+    console.log(`log group: ${logGroupName}`);
 
     // TODO MAX
     // await setExpiry(logGroupName)
-    console.log(`updated [${logGroupName}]'s retention policy to ${retentionDays} days`)
-}
+    console.log(`updated [${logGroupName}]'s retention policy to ${retentionDays} days`);
+};
